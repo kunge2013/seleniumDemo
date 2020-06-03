66b9379166c085f58cab655ddcef6f1e74db1116
Test Git <710784360@qq.com>
2020-06-03 19:33:11 +0800

    1

--- src/main/java/com/selenium/ChainEvent.java
+++ src/main/java/com/selenium/ChainEvent.java
@@ -1,9 +1,30 @@
 package com.selenium;
-
+/**
+ * 事件处理
+ * @author zc
+ *
+ */
 public abstract class ChainEvent {
 	
 	private ChainEvent nextEvent;
 	
 	public abstract void doEvent();
+	
+	public ChainEvent(ChainEvent nextEvent) {
+		this.nextEvent = nextEvent;
+	}
+	
+	public ChainEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChainEvent getNextEvent() {
+		return nextEvent;
+	}
+
+	public void setNextEvent(ChainEvent nextEvent) {
+		this.nextEvent = nextEvent;
+	}
+	
 	
 }
